54cc27001e8763871d9a53f9f0782ed30bebd8ec
Nick Fox <nicholas.f.fox@gmail.com>
2025-09-28 00:03:12 -0400

    set up routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
+
+
+import { createBrowserRouter,  RouterProvider } from 'react-router-dom';
+import AppLayout from './routes/App';
+import Home from './routes/Home';
+import Picks from './routes/Picks';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [
+      { index: true, element: <Home /> }, 
+      { path: 'picks', element: <Picks /> }
+    ],
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
